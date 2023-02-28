90240ab70ba374980862148cd00cc76d177b17b7
BodyYvesEL <bodymoliki@gmail.com>
2023-02-28 18:12:48 +0100

    Generated Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 
 
@@ -27,21 +32,22 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error("Email has already been registered.")
     }
 
-    
-
     // Create new user
 
     const user = await User.create({
         name,
         email, 
         password,
-    })
+    });
+
+    // Generate Token
+    const token = generateToken(user._id)
 
     if (user) {
         const {_id, name, email, photo, phone, bio} = user
         res.status(201).json({
-            _id, name, email, photo, phone, bio 
-        })
+            _id, name, email, photo, phone, bio, token,
+        });
     } else {
         res.status(400)
         throw new Error("Invalid user data")
